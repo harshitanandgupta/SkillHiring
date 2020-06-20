33e81678d6973554cca3e1ea9a8614a58fdd17e5
seriouslearner <harshitanandgupta@gmail.com>
2020-06-21 00:34:52 +0530

    Updates

---
+++ precache-manifest.bfa834e8e037c4adeadbc55305975719.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74479131a320c0bc7d69dd17ad1948ea",
+    "url": "/SkillHiring/index.html"
+  },
+  {
+    "revision": "24b1b9b9911bb5c2bc86",
+    "url": "/SkillHiring/static/css/2.cfa47bc4.chunk.css"
+  },
+  {
+    "revision": "cc65bb661f803a2bee24",
+    "url": "/SkillHiring/static/css/main.e3106115.chunk.css"
+  },
+  {
+    "revision": "24b1b9b9911bb5c2bc86",
+    "url": "/SkillHiring/static/js/2.fcdce2cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SkillHiring/static/js/2.fcdce2cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc65bb661f803a2bee24",
+    "url": "/SkillHiring/static/js/main.264eca07.chunk.js"
+  },
+  {
+    "revision": "7a4470ae3a701310f068",
+    "url": "/SkillHiring/static/js/runtime-main.69610202.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SkillHiring/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SkillHiring/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SkillHiring/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SkillHiring/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SkillHiring/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
